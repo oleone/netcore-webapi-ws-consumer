2f249990aae9de28ac893fcdca48c27ee35eb196
Bruno Leone A P Corrêa <gerencia@acetime.com.br>
2022-02-16 23:45:07 -0300

    Adicionar arquivos de projeto.

---
+++ PUC.ArquiteturaDeSoftwareDistribuido.APIsWebServicesA1/Controllers/CountriesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PUC.ArquiteturaDeSoftwareDistribuido.APIsWebServicesA1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        public CountriesController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult Get ()
+        {
+            try
+            {
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ PUC.ArquiteturaDeSoftwareDistribuido.APIsWebServicesA1/Services/CountriesService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PUC.ArquiteturaDeSoftwareDistribuido.APIsWebServicesA1.Services
+{
+    public class CountriesService
+    {
+        public dynamic GetByName()
+        {
+            return new object { };
+        }
+    }
+}
